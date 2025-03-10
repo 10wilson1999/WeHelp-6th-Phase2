8befbe341aa14e0098321331925c93ac44eff5e1
wilson <10wilson1999@gmail.com>
2025-03-10 16:47:05 +0800

    Modify app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from fastapi import *
-from fastapi.responses import FileResponse
-app=fastapi()
+from fastapi import * # type: ignore
+from fastapi.responses import FileResponse # type: ignore
+app=FastAPI() # type: ignore
 
 # Static Pages (Never Modify Code in this Block)
 @app.get("/", include_in_schema=False)
-async def index(request: Request):
+async def index(request: Request): # type: ignore
 	return FileResponse("./static/index.html", media_type="text/html")
 @app.get("/attraction/{id}", include_in_schema=False)
-async def attraction(request: Request, id: int):
+async def attraction(request: Request, id: int): # type: ignore
 	return FileResponse("./static/attraction.html", media_type="text/html")
 @app.get("/booking", include_in_schema=False)
-async def booking(request: Request):
+async def booking(request: Request): # type: ignore
 	return FileResponse("./static/booking.html", media_type="text/html")
 @app.get("/thankyou", include_in_schema=False)
-async def thankyou(request: Request):
+async def thankyou(request: Request): # type: ignore
 	return FileResponse("./static/thankyou.html", media_type="text/html")
